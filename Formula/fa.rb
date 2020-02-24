b3edb417b87772dacef451516fff5ad661b38609
Krishna Nannuru <nkkarthik@gmail.com>
2020-02-24 03:09:26 -0800

    formula

--- Formula/fa.rb
+++ Formula/fa.rb
@@ -2,7 +2,7 @@ class Fa < Formula
   desc "fa tools"
   homepage "https://github.com/nkkarthik/fa"
   url "https://github.com/nkkarthik/fa", :using => :git
-  version "0.1.30"
+  version "0.1.4"
   #sha256 "b18d6ed4a662f8b9fbf27eaec87b083ba1294fe5e0702e92f2881e3d6af1c33e"
 
   depends_on "elixir"
